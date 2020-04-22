7e5a5271422d2f816d967b703f9429ca8814e064
Frederick-88 <frederickfd88@gmail.com>
2020-04-22 18:44:24 +0700

    FULLSTACK-FRONTEND-INITIALCOMMIT

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Content from './components/Index'
+
+function App() {
+  return (
+   <div>
+     <Content/>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/components/Index.js
@@ -0,0 +1,37 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+
+export default function FetchData() {
+    const [data, setData] = useState([])
+
+    useEffect(()=>{
+        axios.get('http://localhost:8000/heroes/get')
+        .then(response=>{
+            console.log(response.data.data);
+            
+            // setData({data: response.data})
+
+            
+            setData(response.data.data)
+        })
+    },[])
+
+
+        return (
+            <div>
+                <h1>DATA PAHLAWAN NASIONAL</h1>
+               {
+                data.map(item =>
+                    <ul>
+                        <h2> Pahlawan </h2>
+                <li>{item.name}</li>
+                <li>{item.born}</li>
+                <li>{item.dead}</li>
+                <li>{item.description}</li>
+                <li>{item.establishment}</li>
+                </ul>
+                )
+               }
+            </div>
+        )
+        }
